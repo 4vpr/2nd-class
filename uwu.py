af50b59f849c961f2ee4fc3aed0eda58f78235f0
h4yase <4shout4@gmail.com>
2025-08-21 19:19:23 +0900

    new

---
+++ uwu.py
@@ -0,0 +1,16 @@
+
+word = "Python"
+print(word[3])
+print(word[-1])
+
+numbers = [10, 20, 30, 40, 50]
+print(numbers[2])
+print(numbers[0])
+
+text = "Python"
+print(text[0:3])
+print(text[2:])
+
+data = [10, 20, 30, 40, 50]
+print(data[1:4])
+print(data[::-1])
